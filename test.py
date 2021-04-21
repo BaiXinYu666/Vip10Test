7fbcd2e4952bfa2aa49d704a2450d3bb4e768f37
15535496803@163.com <15535496803@163.com>
2021-04-21 18:48:02 +0800

    作业提交

--- test.py
+++ test.py
@@ -57,6 +57,19 @@ del duixiang
 # 定义一个老师类，老师又姓名，性别，课程 方法可以教学，
 # 实现一段话：什么什么老师性别是什么什么，教什么什么课程
 # 先定义一个老师类
+
+class  laoshi():
+    def __init__(self,xingming,age,course):
+        self.xingming = xingming
+        self.age = age
+        self.course = course
+    def dongci(self):
+        print(f'姓名是:{self.xingming}，性别是{self.age},课程是{self.course}')
+aa = laoshi('aaa',16,'yuwen')
+aa.dongci()
+print(aa)
+
+
 '''class Teacher():
     def __init__(self, xingming, xingbie, course):
         self.xingming = xingming
@@ -77,24 +90,24 @@ duixaing.dongci()
 print(duixaing)'''
 
 #房子类  属性：面积  剩余面积  家具列表
-class jiaju():
-    def __init__(self,jiajuname,mianji):
-        self.jiajuname=  jiajuname
-        self.mianji = mianji
-class fangzi():
-    def __str__(self):
-        return f'面积是{self.mianji},剩余面积是{self.shengyumianji}'
-    def __init__(self,mianji):
-        self.mianji = mianji
-        self.shengyumianji = mianji
-        self.list = []
-    def rongna(self):
-        if self.shengyumianji>self.mianji:
-            self.list.append(self.jiajuname)
-        else:
-            print('空间不足了')
-bb = fangzi(100)
-bb.rongna()
+# class jiaju():
+#     def __init__(self,jiajuname,mianji):
+#         self.jiajuname=  jiajuname
+#         self.mianji = mianji
+# class fangzi():
+#     def __str__(self):
+#         return f'面积是{self.mianji},剩余面积是{self.shengyumianji}'
+#     def __init__(self,mianji):
+#         self.mianji = mianji
+#         self.shengyumianji = mianji
+#         self.list = []
+#     def rongna(self):
+#         if self.shengyumianji>self.mianji:
+#             self.list.append(self.jiajuname)
+#         else:
+#             print('空间不足')
+# bb = fangzi(100)
+# bb.rongna()
 
 
 
